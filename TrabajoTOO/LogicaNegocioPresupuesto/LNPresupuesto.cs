621c207b6f07688d5c5872190e796ea67cffb042
jugarcca <jugarcca@unirioja.loc>
2021-01-13 13:46:49 +0100

    Dejarlo bonito

--- TrabajoTOO/LogicaNegocioPresupuesto/LNPresupuesto.cs
+++ TrabajoTOO/LogicaNegocioPresupuesto/LNPresupuesto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaPersistenciaPresupuesto
+namespace LogicaNegocioPresupuesto
 {
-    class PresupuestoDato
+    public class Class1
     {
     }
 }
