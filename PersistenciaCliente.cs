923c6d4f6853c445e11c5aabf6b94ce1a5236491
vicoman99 <43779372+vicoman99@users.noreply.github.com>
2021-01-13 12:27:22 +0100

    Clases
    todas las clases

---
+++ PersistenciaCliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac10
+{
+    class PersistenciaCliente
+    {
+        public static String conex;
+        public static void CREATE_CLIENTE (ClienteDato c)
+        {
+            BD.INSERTCliente(c);
+        }
+
+        public static bool EXISTE (ClienteDato c)
+        {
+            bool existe = false;
+
+            foreach(ClienteDato cliente in BD.Clientes)
+            {
+                if (cliente.Equals(c))
+                {
+                    existe = true;
+                }
+            }
+            return existe;
+        }
+    }
+}
